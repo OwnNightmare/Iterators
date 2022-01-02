e0394b9b643b931e66762ef6026f5a24da2dfb9e
OwnNightmare <yura.borisov.9523@gmail.com>
2022-01-02 22:33:07 +0300

    Iterator works

--- main.py
+++ main.py
@@ -1,19 +1,74 @@
-from typing import Any
+import time
 
 
-def my_iter(pack: list):
-    for i in pack:
-        if type(i) == list:
-            for s in i:
-                yield s
+ex = [1, 2, 3, ['foo', 'bar', 'duck'], 18, [['nested element', [[['crazy nested element']]]]]]
+nested_lists = [[1, 2, 3, 4], ['one', 'two', 'three', 'four'], True]
+
+
+def my_generator(pack: list):
+    for elem in pack:
+        if isinstance(elem, list):
+            for nested_elem in my_generator(elem):
+                yield nested_elem
         else:
-            yield i
+            yield elem
 
 
-ex = [1, 2, 3, None, ['foo', 'bar'], 18, ['some nested element']]
-for item in (my_iter(ex)):
-    print(item)
+def call_generator():
+    for item in (my_generator(ex)):
+        print(item)
+
+    flatted = [i for i in my_generator(ex)]
+    print(flatted)
+    print('This was generator')
 
 
-flatted = [i for i in my_iter(ex)]
-print(flatted)
+call_generator()
+
+
+class MyIterator:
+
+    def __init__(self, obj: list):
+        self.obj = obj
+        self.flat = []
+        self.cursor = 0
+        self.limit = len(obj)
+
+    def __iter__(self, data: 'omit or List' = None):
+        if data is None:
+            for el in self.obj:
+                if isinstance(el, list):
+                    self.__iter__(el)
+                else:
+                    self.flat.append(el)
+            return self.flat.__iter__()
+        else:
+            for el in data:
+                if isinstance(el, list):
+                    self.__iter__(el)
+                else:
+                    self.flat.append(el)
+            return self.flat.__iter__()  # == iter(self.flat)
+
+    def __next__(self):
+        if self.cursor >= self.limit:
+            raise StopIteration
+        self.item = self.obj[self.cursor]
+        self.cursor += 1
+        if len(self.obj) == 0:
+            return self.obj
+        return self.item
+
+
+mine = MyIterator(ex)
+for i in mine:
+    print(i)
+    time.sleep(0.5)
+
+li = [i for i in MyIterator(ex)]
+print(li)
+
+
+
+
+
